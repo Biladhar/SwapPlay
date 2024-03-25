24822d1325f0c275ef749495e04040839d7e86df
Khalil Ben Othman <kbenothman9@gmail.com>
2024-03-25 04:53:53 +0100

    25/03/2024
    details ,marketplace,offer

--- flask_app/controllers/game_controller.py
+++ flask_app/controllers/game_controller.py
@@ -1,44 +1,100 @@
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> market_place
 from flask_app import app
 from flask import redirect, render_template, request, flash, session
 from flask_app.models.games import Game
 from flask_app.models.users import User
+from werkzeug.utils import secure_filename
+import os
 
 
 
-<<<<<<< HEAD
+
 # * View Route
 @app.route("/marketplace")
 def marketplace():
-    return render_template("marketplace.html")
+    games = Game.get_all_games()
+    return render_template("marketplace.html" ,games = games)
 
-=======
->>>>>>> market_place
-# * view route for the add new game form
-@app.route("/game/new")
-def add_game():
+UPLOAD_FOLDER = 'C:/Users/kbeno/Desktop/py_project/SwapPlay/flask_app/static/images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/game/add", methods=["GET","POST"])
+def new_game():
+
+        if request.method == 'POST':
+        # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                # update user data in the database
+                data = { 
+                    **request.form,
+                    "user_id":session["user_id"],
+                    "image": filename
+                    
+                }
+                Game.add(data)
+                return redirect('/dashboard')
+        return render_template('new_game.html')
+
+# ? =============   EDIT GAME   ==========================
+# * view route edit game
+@app.route("/game/edit/<int:id>")
+def edit_game(id):
     if "user_id" not in session:
         return redirect ("/")
-    return render_template("new_game.html")
+    data={
+        "id" : id
+    }
+    game = Game.get_one_game_with_id(data)
+    return render_template("edit_game.html",game=game)
 
-# ! action route to create a new game
-@app.route("/game/add", methods=["POST"])
-def new_game():
-        if not Game.validate_game(request.form):
-            return redirect("/game/new")
-        data = {    
+# ! action route edit game
+@app.route('/game/process/<int:id>', methods = ['POST'])
+def edit_one_game(id):
+    if not Game.validate_game(request.form):
+        return redirect(f"/game/edit/{id}")
+    data = {
         **request.form,
-        "user_id":session["user_id"]
-        }
-        Game.add(data)
-<<<<<<< HEAD
-        return redirect("/dashboard")
-=======
-        return redirect("/dashboard")
+        "id" : id
+    }
+
+    Game.edit_game(data)
+    return redirect("/dashboard")
+
+# # * view route for the add new game form
+# @app.route("/game/new")
+# def add_game():
+#     if "user_id" not in session:
+#         return redirect ("/")
+#     return render_template("new_game.html")
+
+
+# # ! action route to create a new game
+# @app.route("/game/add", methods=["GET","POST"])
+# def new_game():
+#         if not Game.validate_game(request.form):
+#             return redirect("/game/new")
+        
+#         data = {    
+#         **request.form,
+#         "user_id":session["user_id"]
+#         }
+#         Game.add(data)
+#         return redirect("/dashboard")
 
 @app.route("/marketplace/show_games")
 def show_all():
@@ -62,5 +118,27 @@ def show_state():
     user = User.get_by_id(data)
     state_games = Game.get_all_state({"state" : request.form["state"]})
     return render_template("marketplace_state.html", state_games=state_games,user=user)
->>>>>>> Stashed changes
->>>>>>> market_place
+
+# * make offer
+@app.route("/game/offer/<int:id>")
+def offer(id):
+    if "user_id" not in session:
+        return redirect ("/")
+    data={
+        "id" : id
+    }
+    game1 = Game.get_one_game_with_user(data)
+    data = {
+        **request.form,
+        "user_id": session["user_id"]
+        }
+    offers = Game.get_one_user_games(data)
+    return render_template("offer.html",game1 =game1, offers=offers)
+
+@app.route("/delete/<int:id>")
+def delete_game_(id):
+    data={
+        "id" : id
+    }
+    Game.delete_game_user(data)
+    return redirect("/dashboard")
--- flask_app/models/games.py
+++ flask_app/models/games.py
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> market_place
+
 from flask_app.configs.mysqlconnection import connectToMySQL
 from flask_app import DATABASE
 from flask import flash
+from pprint import pprint
+from flask_app.models import users
+from flask_app.models.users import User
 
 class Game:
     def __init__(self, data) :
@@ -27,7 +26,51 @@ class Game:
                 """
         return connectToMySQL(DATABASE).query_db(query, data)
     
-# * get all the game of one user
+    # ******************   READ ALL GAMES **************************************
+
+    @classmethod
+    def get_all_games(cls):
+
+        query = "SELECT * FROM games;"
+
+        results = connectToMySQL(DATABASE).query_db(query)
+
+        # print(results)
+
+        games_instances = []
+        if results:
+            for row in results:
+                one_game = Game(row)
+                games_instances.append(one_game)
+
+            return games_instances
+        
+        return []
+    
+    # ******************   EDIT GAME **************************************
+    @classmethod
+    def edit_game(cls,data):
+        query="""
+                UPDATE games
+                SET name=%(name)s, state=%(state)s, image= %(image)s, platform=%(platform)s
+                WHERE id= %(id)s;
+                """
+        return connectToMySQL(DATABASE).query_db(query,data)
+    
+    # ********************* GET ONE GAME WITH ID ***********************
+    @classmethod
+    def get_one_game_with_id(cls, data):
+
+        query = """
+                    SELECT * FROM games
+                    WHERE games.id = %(id)s;
+                """
+        
+        result = connectToMySQL(DATABASE).query_db(query, data)
+        return Game(result[0])
+    
+
+# ********************** GET ALL GAMES OF 1 USER ***********************
     @classmethod
     def get_one_user_games(cls, data2):
 
@@ -41,20 +84,6 @@ class Game:
         if len(result) < 1:
             return no_game
         return result
-<<<<<<< HEAD
-=======
-    
-    
-    @classmethod
-    def get_all_games(cls):
-        query = """
-                SELECT * FROM games;
-                """
-        results = connectToMySQL(DATABASE).query_db(query)
-        all_games =[]
-        for games in results :
-            all_games.append(cls(games))
-        return all_games
     
     @classmethod
     def get_all_state(cls, state):
@@ -69,22 +98,55 @@ class Game:
             state_games.append(cls(state))
         
         return state_games
->>>>>>> market_place
 
+    
+# ******************   READ GAME WITH USER **************************************
+    @classmethod
+    def get_one_game_with_user(cls, data):
+
+        query = """
+                    SELECT * FROM users
+                    JOIN games ON users.id = games.user_id
+                    WHERE games.id = %(id)s;
+                """
         
+        results = connectToMySQL(DATABASE).query_db(query, data)
+        pprint(results)
+        this_game = Game(results[0])
+        this_game.user = User(results[0])
+        print(this_game)        
+        return this_game
 
+    @classmethod
+    def get_all_games(cls):
+        query = """
+                SELECT * FROM users
+                JOIN games ON users.id = games.user_id
+                    
+                """
+        results = connectToMySQL(DATABASE).query_db(query)
+        print(f"RECIPES_WITH_USERS: ----{results}")
+        all_games =[]
+        for x in results:
+            game = cls(x)
+            data = {
+                **x,
+                "id":x['games.id'],
+                "created_at":x['games.created_at'],
+                "updated_at":x['games.updated_at']
+            }
+            game.posted_by = users.User(data)
+            game.gameid= users.User(data)
+            all_games.append(game)
+        return all_games
 
-
-
-
-
-
-
-
-
-
-
-
+    @classmethod
+    def delete_game_user(cls,data):
+        query="""
+                DELETE FROM games 
+                WHERE id=%(id)s
+                """
+        return connectToMySQL(DATABASE).query_db(query,data)
 
 
 
@@ -100,13 +162,9 @@ class Game:
         if len(data["state"]) < 3 :
             is_valid = False
             flash("state is required", "game")
-        if len(data["image"]) < 3:
-            is_valid = False
-            flash("image is required", "game")
+        # if len(data["image"]) < 3:
+        #     is_valid = False
+        #     flash("image is required", "game")
 
-<<<<<<< HEAD
         return is_valid
-=======
-        return is_valid
->>>>>>> Stashed changes
->>>>>>> market_place
+
